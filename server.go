5f4605d2f7f2188ab3c1762f98c9c37959dce6f4
agatan <agatan039@gmail.com>
2017-04-05 23:00:04 +0900

    Add HTTPS proxy support

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/pkg/errors"
@@ -50,8 +51,48 @@ func copyResponse(dst http.ResponseWriter, src *http.Response) error {
 	return nil
 }
 
+func (p *ProxyServer) pipeConn(dst, src *net.TCPConn) {
+	if _, err := io.Copy(dst, src); err != nil {
+		p.logf("failed to pipe connections: %v", err)
+	}
+	dst.CloseWrite()
+	src.CloseRead()
+}
+
+func (p *ProxyServer) handleHTTPS(w http.ResponseWriter, r *http.Request) {
+	hij, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "cannot hijack https request", http.StatusInternalServerError)
+		return
+	}
+	cliConn, _, err := hij.Hijack()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to hijack https connection: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	cliConn.Write([]byte("HTTP/1.0 200 OK \r\n\r\n"))
+
+	dstConn, err := net.Dial("tcp", r.URL.Host)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to connect the destination server: %v", err), http.StatusBadGateway)
+		return
+	}
+	dstTCPConn := dstConn.(*net.TCPConn)
+	cliTCPConn := cliConn.(*net.TCPConn)
+
+	go p.pipeConn(dstTCPConn, cliTCPConn)
+	go p.pipeConn(cliTCPConn, dstTCPConn)
+
+	p.logf("accept CONNECT to %s", r.URL.Host)
+}
+
 func (p *ProxyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	p.logf("received request: %#v", r)
+	if r.Method == "CONNECT" {
+		p.handleHTTPS(w, r)
+		return
+	}
 	if !r.URL.IsAbs() {
 		if p.Handler == nil {
 			http.Error(w, "cannot handle non-proxy requests", http.StatusBadRequest)
