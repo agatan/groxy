8d924e318abcbc226479b8204381945b4695708a
agatan <agatan039@gmail.com>
2017-10-07 22:21:15 +0900

    Update example

--- _example/groxy-proxy/main.go
+++ _example/groxy-proxy/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/agatan/groxy"
 )
 
+type logger struct{}
+
+func (logger) Print(args ...interface{}) {
+	log.Println(args...)
+}
+
 func main() {
 	proxy := groxy.New()
+	proxy.Logger = logger{}
 	if err := http.ListenAndServe(":8888", proxy); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
